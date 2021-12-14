20a52478f2e4e521b2bdef794ac01b188efe7136
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-12-14 18:51:23 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,70 @@
 import "./styles.css";
-import * as THREE from 'three';
-import {useEffect} from 'react';
-import {Canvas, useFrame, useThree} from '@react-three/fiber';
-import {Physics, usePlane, useCompountBody, useSphere} from '@react-three/cannon'
+import * as THREE from "three";
+import { useEffect } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import {
+  Physics,
+  usePlane,
+  useCompoundBody,
+  useSphere
+} from "@react-three/cannon";
+import { Environment, useGLTF } from "@react-three/drei";
+import { EffectComposer, SSAO } from "@react-three/postprocessing";
 
+const baubleMaterial = new THREE.MeshLambertMaterial({
+  color: "#c0a090",
+  emissive: "red"
+});
+const capMaterial = new THREE.MeshStandardMaterial({
+  metalness: 1,
+  roughness: 0.15,
+  color: "##8a300f",
+  emissive: "#600000",
+  envMapIntensity: 9
+});
+
+const sphereGeometry = new THREE.SphereGeometry(1, 28, 28);
+const baubles = [...Array(50)].map(() => ({
+  args: [0.6, 0.6, 0.8, 0.8, 1][Math.floor(Math.random() * 5)],
+  mass: 1,
+  angularDamping: 0.2,
+  linearDamping: 0.95
+}));
+
+function Bauuble({ vec = new THREE.Vector3(), ...props }) {
+  const { nodes } = useGLTF("cap.glb");
+  const [ref, api] = useCompoundBody(() => ({
+    ...props,
+    shapes: [
+      {
+        type: "Box",
+        position: [0, 0, 1.2 * props.args],
+        args: new THREE.Vector3().setScalar(props.args * 0.4).toArray()
+      },
+      { type: "Sphere", args: [props.args] }
+    ]
+  }));
+  useEffect(
+    () =>
+      api.position.subscribe((p) =>
+        api.applyForce(
+          vec
+            .set(...p)
+            .normaalize()
+            .multiplyScalar(-props.args * 35)
+            .toArray(),
+          [0, 0, 0]
+        )
+      ),
+    [api]
+  );
+
+  return (
+    <group>
+      <mesh />
+    </group>
+  );
+}
 export default function App() {
   return (
     <div className="App">
